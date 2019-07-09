b31af9930d70644d2da2480013bc58981a942f53
lim kuk tai <jnssanaee@nate.com>
2019-07-09 22:55:22 +0900

    ADD : input value 값으로 더하기 (진행중)

--- src/App.js
+++ src/App.js
@@ -6,13 +6,14 @@ class App extends React.Component {
     super(props);
     this.state = {
       count: 0,
+      number: ''
     };
   }
 
   increment = () => {
-    this.setState(({ count }) => ({
-      // count: this.state.count + 1 과 선언 시 'this.state'를 또 조회한다.
-      count: count + 1,
+    this.setState(({ count }) => ({ // count: this.state.count + 1 과 선언 시 'this.state'를 또 조회한다.
+    //count: count - 1,  
+    count: count + this.state.number, 
     }));
   };
   decrement = () => {
@@ -22,11 +23,17 @@ class App extends React.Component {
   };
   reset = () => this.setState({ count: 0 });
 
+  handleChange = (e) => {
+    this.setState({
+      number: parseInt(e.target.value)
+    })
+  }
+
   render() {
     return (
       <div>
         <div className="count">{this.state.count}</div>
-        <input className="form-control" type="text" id="input_num" placeholder="Enter number" />
+        <input className="form-control" type="text" id="input_num" value={this.state.number} onChange={this.handleChange} placeholder="Enter number" />
         <div>
           <button type="button" className="btn btn-primary" onClick={this.increment} title="plus">
             +
