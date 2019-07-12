bc83d079f8c760373eedf824f49baf4e0bf5e41f
lim kuk tai <jnssanaee@nate.com>
2019-07-12 16:07:24 +0900

    REFAC : 버튼 컴포넌트 '하드코딩 => map을 이용한 재활용'

--- src/components/BtnGroup.js
+++ src/components/BtnGroup.js
@@ -1,41 +1,17 @@
 import React from 'react';
 
 function BtnGroup({increment, decrement, reset}) {
-    const btnJson = [
-        { txt: '+', class: 'btn-primary', onclick: 'increment', title: 'plus' },
-        { txt: '-', class: 'btn-danger', onclick: 'decrement', title: 'minus' },
-        { txt: 'reset', class: 'btn-outline-secondary', onclick: 'reset', title: '' },
+    const btnArr = [
+        { txt: '+', class: 'btn btn-primary', onclick: increment, title: 'plus' },
+        { txt: '-', class: 'btn btn-danger', onclick: decrement, title: 'minus' },
+        { txt: 'reset', class: 'btn btn-outline-secondary', onclick: reset },
     ]
-    const btnList = btnJson.map((element, index) => {
-        (<button className="btn" key={index}></button>)
-    })
 
     return (
         <div>
-            {btnList}
-            {/* <button 
-                type="button"
-                className="btn btn-primary"
-                onClick={increment}
-                title="plus"
-            >
-                +
-            </button>
-            <button 
-                type="button" 
-                className="btn btn-danger" 
-                onClick={decrement} 
-                title="minus"
-            >
-                -
-            </button>
-            <button 
-                type="button" 
-                className="btn btn-outline-secondary" 
-                onClick={reset}
-            >
-                reset
-            </button> */}
+            {btnArr.map((e, index) => {
+                return <button type="button" key={index} className={e.class} onClick={e.onclick} title={e.title}>{e.txt}</button>
+            })}
         </div>
     )
 }
